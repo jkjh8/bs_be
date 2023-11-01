d30a15b92045ec0abcd5ca4c30f945855ceff802
jkjh8 <jkjh82@naver.com>
2023-11-01 17:14:01 +0900

    uploaded

--- routes/api/files/index.js
+++ routes/api/files/index.js
@@ -1,10 +1,25 @@
 import express from 'express'
 import path from 'node:path'
 import fs from 'node:fs'
+import multer from 'multer'
 import { logInfo, logError } from '@/api/logger'
+
 const router = express.Router()
 
 const mediaPath = path.resolve(__dirname, '../../../media')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const { folders } = req.headers
+    cb(null, path.resolve(mediaPath, folders))
+  },
+  filename: (req, file, cb) => {
+    console.log(file)
+    cb(null, file.fieldname.toString('utf8'))
+  }
+})
+
+const upload = multer({ storage: storage })
 
 router.get('/', (req, res) => {
   try {
@@ -25,7 +40,7 @@ router.get('/', (req, res) => {
     }
     res.status(200).json({ files: filesWithData })
   } catch (error) {
-    logError(`get media files error: ${error}`)
+    logError(`get media files error: ${error}`, 'server', 'files')
     res.status(500).json({ error })
   }
 })
@@ -37,10 +52,31 @@ router.post('/newfolder', (req, res) => {
     if (!fs.existsSync(newFolder)) {
       fs.mkdirSync(newFolder)
     }
+    logInfo(`make new folder ${name} by ${req.user.email}`, 'server', 'files')
     res.status(200).json({ result: 'OK' })
   } catch (error) {
-    logError(`make new folder error: ${error}`)
+    logError(`make new folder error: ${error}`, 'server', 'files')
     res.status(500).json({ error })
   }
 })
+
+router.delete('/remove', (req, res) => {
+  try {
+    const { fileFullPath } = req.body
+    if (fs.existsSync(fileFullPath)) {
+      fs.rmSync(fileFullPath, { recursive: true })
+    }
+    res.status(200).json({ result: 'OK' })
+  } catch (error) {
+    logError(`remove folder or file error ${error}`, 'server', 'files')
+  }
+})
+
+router.post('/', upload.any(), (req, res) => {
+  res.status(200).json({
+    result: 'OK',
+    files: req.file
+  })
+})
+
 export default router
